6ac4fda1db81c347ac70b887bdf4415a1a2d3364
Oliver Pirkfellner <oliver.pirkfellner.dev@gmail.com>
2016-11-22 14:57:53 +0100

    checkpoint-23-complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121091358) do
+ActiveRecord::Schema.define(version: 20161122131358) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20161121091358) do
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"

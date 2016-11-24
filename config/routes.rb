06ab91e6c2b27396b408f3f7d46342e396ca4bae
Oliver Pirkfellner <oliver.pirkfellner.dev@gmail.com>
2016-11-24 11:44:48 +0100

    checkpoint26complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
